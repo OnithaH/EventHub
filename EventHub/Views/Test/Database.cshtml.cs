c3e9a01f5e67b901f3ca8e6e1d67024fc39bac3d
Onitha <onithahettiarachchi26@gmail.com>
2025-09-18 11:29:25 +0530

    New

---
+++ EventHub/Views/Test/Database.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventHub.Views.Test
+{
+    public class DatabaseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EventHub/Views/Test/Insert.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventHub.Views.Test
+{
+    public class InsertModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
